c85094c8cfffbc8b2bf63c57335efe3e3c0e09b7
Loïc Dutrieux <loic.dutrieux@gmail.com>
2024-08-20 14:46:07 +0200

    Function to access zarr store on open data repository

--- nrt/data/__init__.py
+++ nrt/data/__init__.py
@@ -15,7 +15,7 @@
 
 import os
 import json
-import warning
+import warnings
 
 import xarray as xr
 import rasterio
@@ -86,6 +86,59 @@ def romania_20m(**kwargs):
         >>> s2_cube = s2_cube.where(s2_cube.SCL.isin([4,5,7]))
     """
     return _load('sentinel2_cube_subset_romania_20m.nc', **kwargs)
+
+
+def germany_zarr(**kwargs):
+    """Sentinel 2 datacube of a forest area affected by Bark Beetle in Germany
+
+    The data covers an area of 400 km\ :sup:`2` located East of the city Cologne
+    in the North Rhine-Westphalia state of Germany. It spans a five year period
+    from 2018 to 2022 and includes the sentinel bands B02, B03, B04, B08, B11,
+    B12 and SCL. Native 10m resolution is preserved for the visible and near-infrared
+    bands while B11, B12 and SCL were resampled to 10m from their original 20m resolution.
+    The data is organized as an online zarr store on the JRC's Open Data Repository,
+    so that running the ``germany_zarr()`` function only creates a symbolic representation
+    of the dataset (dask based). Lazy chunks can be eagerly loaded by invoking the
+    ``.compute()`` method. Beware that loading the entire dataset may take a fairly
+    long time due to its size (4.9 GB).
+
+    Args:
+        **kwargs: Additional keyword arguments passed to ``xarray.open_zarr()``
+
+    Examples:
+        >>> import sys
+        >>> from nrt import data
+        >>> ds = data.germany_zarr()
+        >>> print(ds)
+        <xarray.Dataset>
+        Dimensions:      (time: 172, y: 2000, x: 2000)
+        Coordinates:
+            spatial_ref  int32 ...
+          * time         (time) datetime64[ns] 2018-02-14T10:31:31.026000 ... 2022-12...
+          * x            (x) float64 4.133e+06 4.133e+06 ... 4.153e+06 4.153e+06
+          * y            (y) float64 3.113e+06 3.113e+06 ... 3.093e+06 3.093e+06
+        Data variables:
+            B02          (time, y, x) uint16 dask.array<chunksize=(10, 100, 100), meta=np.ndarray>
+            B03          (time, y, x) uint16 dask.array<chunksize=(10, 100, 100), meta=np.ndarray>
+            B04          (time, y, x) uint16 dask.array<chunksize=(10, 100, 100), meta=np.ndarray>
+            B08          (time, y, x) uint16 dask.array<chunksize=(10, 100, 100), meta=np.ndarray>
+            B11          (time, y, x) uint16 dask.array<chunksize=(10, 100, 100), meta=np.ndarray>
+            B12          (time, y, x) uint16 dask.array<chunksize=(10, 100, 100), meta=np.ndarray>
+            SCL          (time, y, x) uint8 dask.array<chunksize=(10, 100, 100), meta=np.ndarray>
+        >>> # Load a subset in memory
+        >>> print(sys.getsizeof(ds.B02.data))
+        80
+        >>> ds_sub = ds.isel(x=slice(100,110), y=slice(200,210), time=slice(10,20)).compute()
+        >>> print(sys.getsizeof(ds_sub.B02.data))
+        2144
+
+    Returns:
+        xarray.Dataset: A dask based Dataset of dimension ``(time: 172, y: 2000, x: 2000)``
+        with 7 data variables.
+    """
+    ds = xr.open_zarr('https://jeodpp.jrc.ec.europa.eu/ftp/jrc-opendata/FOREST/NRT/NRT-DATA/VER1-0/germany.zarr',
+                      **kwargs)
+    return ds
 
 
 def romania_forest_cover_percentage():
